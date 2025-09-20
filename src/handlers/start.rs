7f09d0f8033b9362d568c37f63feb9b4cc984f9c
Andriy <114863893+noinsts@users.noreply.github.com>
2025-09-20 11:35:33 +0300

    Update start_handler: add Db dependency and check user existence

--- src/handlers/start.rs
+++ src/handlers/start.rs
@@ -1,18 +1,18 @@
 use teloxide::prelude::*;
+use crate::db::db::Db;
+use crate::db::queries::user_exists;
 
 /// Handler of /start command
-pub async fn start_handler(bot: Bot, msg: Message) -> ResponseResult<()> {
-    let text = if let Some(user) = &msg.from() {
-        format!("👋 Hello, {}! Welcome to the weather bot!", user.first_name)
-    }
-    else {
-        "👋 Hello! Welcome to the weather bot!".to_string()
-    };
+pub async fn start_handler(bot: Bot, msg: Message, db: Db) -> ResponseResult<()> {
+    if let Some(user) = msg.from {
+        let text = if user_exists(&db, user.id.0 as i64) {
+            "Registered"
+        }
+        else {
+            "No"
+        };
 
-    if let Err(e) = bot.send_message(msg.chat.id, text)
-        .await
-    {
-        eprintln!("Error sending message: {:?}", e);
+        bot.send_message(msg.chat.id, text).await?;
     }
     Ok(())
 }
