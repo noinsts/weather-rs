c32c181b1720085b5a902d9f3c2d85416f9d7704
Andriy <114863893+noinsts@users.noreply.github.com>
2025-09-20 11:36:03 +0300

    feat: inject Db into handle_command and pass to start_handler

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,14 +1,16 @@
 use teloxide::prelude::*;
 use crate::commands::Commands;
+use crate::db::db::Db;
 
 pub mod start;
 
 pub async fn handle_command(
     bot: Bot,
     message: Message,
-    commands: Commands
+    commands: Commands,
+    db: Db
 ) -> ResponseResult<()> {
     match commands {
-        Commands::Start => start::start_handler(bot, message).await,
+        Commands::Start => start::start_handler(bot, message, db).await,
     }
 }
