7a2040e015bcf00a1e485e7f6929b2793ca839a7
Andriy <114863893+noinsts@users.noreply.github.com>
2025-09-20 21:09:27 +0300

    feat: update start_handler to use dialogue and prompt unregistered users

--- src/handlers/start.rs
+++ src/handlers/start.rs
@@ -1,18 +1,23 @@
 use teloxide::prelude::*;
+
 use crate::db::db::Db;
 use crate::db::queries::user_exists;
+use crate::states::State;
+use crate::types::{HandlerResult, MyDialogue};
 
 /// Handler of /start command
-pub async fn start_handler(bot: Bot, msg: Message, db: Db) -> ResponseResult<()> {
+pub async fn start_handler(bot: Bot, msg: Message, dialogue: MyDialogue, db: Db) -> HandlerResult {
     if let Some(user) = msg.from {
-        let text = if user_exists(&db, user.id.0 as i64) {
-            "Registered"
+        if user_exists(&db, user.id.0 as i64) {
+            bot.send_message(msg.chat.id, "Hello!")
+                .await?;
         }
         else {
-            "No"
-        };
-
-        bot.send_message(msg.chat.id, text).await?;
+            bot.send_message(msg.chat.id, "Hello, please, enter ur hometown.")
+                .await?;
+            dialogue.update(State::ReceiveCity)
+                .await?;
+        }
     }
     Ok(())
 }
