95b71c2d754ec2756653587c638a0d48e17ca921
Andriy <114863893+noinsts@users.noreply.github.com>
2025-09-20 21:11:22 +0300

    chore: remove handle_command in favor of new schema-based routing

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,16 +1,2 @@
-use teloxide::prelude::*;
-use crate::commands::Commands;
-use crate::db::db::Db;
-
 pub mod start;
-
-pub async fn handle_command(
-    bot: Bot,
-    message: Message,
-    commands: Commands,
-    db: Db
-) -> ResponseResult<()> {
-    match commands {
-        Commands::Start => start::start_handler(bot, message, db).await,
-    }
-}
+pub mod receive_city;
