493b8d5aa9e3bedcd57fb6c8c99598a3f8946d7c
Andriy <114863893+noinsts@users.noreply.github.com>
2025-09-21 12:04:56 +0300

    feat(schema): add callback_queries branch

--- src/schema.rs
+++ src/schema.rs
@@ -2,10 +2,9 @@ use teloxide::dispatching::{dialogue, UpdateHandler};
 use teloxide::dispatching::dialogue::InMemStorage;
 use teloxide::dptree;
 use teloxide::prelude::*;
-use teloxide::types::Message;
 
 use crate::commands::Commands;
-use crate::handlers::{receive_city, start};
+use crate::handlers::{receive_city, start, today};
 use crate::states::State;
 
 pub fn schema() -> UpdateHandler<Box<dyn std::error::Error + Send + Sync + 'static>> {
@@ -14,10 +13,20 @@ pub fn schema() -> UpdateHandler<Box<dyn std::error::Error + Send + Sync + 'stat
     let commands = teloxide::filter_command::<Commands, _>()
         .branch(case![Commands::Start].endpoint(start::start_handler));
 
-    let dialogue = dialogue::enter::<Message, InMemStorage<State>, State, _>()
-        .branch(commands)
-        .branch(case![State::ReceiveCity].endpoint(receive_city::receive_city_handler));
+    let dialogue = dialogue::enter::<Update, InMemStorage<State>, State, _>()
+        .branch(
+            Update::filter_message()
+                .branch(commands)
+                .branch(case![State::ReceiveCity].endpoint(receive_city::receive_city_handler))
+        );
 
-    Update::filter_message()
+    let callback_queries = Update::filter_callback_query()
+        .branch(
+            dptree::filter(|q: CallbackQuery| q.data.as_deref() == Some("today"))
+                .endpoint(today::today_handler),
+        );
+
+    dptree::entry()
         .branch(dialogue)
+        .branch(callback_queries)
 }
