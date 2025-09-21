44e4edfeba454e135f4567a10bfbb37e2fc6de59
Andriy <114863893+noinsts@users.noreply.github.com>
2025-09-21 17:14:51 +0300

    refactor(keyboard): use `Callbacks` enum instead of hardcoded strings

--- src/utils/keyboard.rs
+++ src/utils/keyboard.rs
@@ -1,8 +1,10 @@
 use teloxide::types::{InlineKeyboardButton, InlineKeyboardMarkup};
 
+use crate::callbacks::Callbacks;
+
 pub fn get_hub_keyboard() -> InlineKeyboardMarkup {
     InlineKeyboardMarkup::new(vec![
-        vec![InlineKeyboardButton::callback("Button 1", "today")],
-        vec![InlineKeyboardButton::callback("Button 2", "tomorrow")],
+        vec![InlineKeyboardButton::callback("Button 1", Callbacks::Today.as_str())],
+        vec![InlineKeyboardButton::callback("Button 2", Callbacks::Tomorrow.as_str())],
     ])
 }
