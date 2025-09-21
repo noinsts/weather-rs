e6e489717e4b980486ad5e62c42bac847196beab
Andriy <114863893+noinsts@users.noreply.github.com>
2025-09-21 12:02:15 +0300

    feat: save user city to DB in receive_city_handler

--- src/handlers/receive_city.rs
+++ src/handlers/receive_city.rs
@@ -3,16 +3,20 @@ use teloxide::Bot;
 use teloxide::types::Message;
 
 use crate::db::db::Db;
+use crate::db::queries::upsert_city;
 use crate::types::{HandlerResult, MyDialogue};
 
 pub async fn receive_city_handler(bot: Bot, dialogue: MyDialogue, msg: Message, db: Db) -> HandlerResult {
-    match msg.text() {
-        Some(city) => {
-            bot.send_message(msg.chat.id,"Ok").await?;
-            let _ = dialogue.exit().await;
-        }
-        None => {
-            bot.send_message(msg.chat.id, "No city specified").await?;
+    if let Some(user) = &msg.from {
+        match msg.text() {
+            Some(city) => {
+                upsert_city(&db, user.id.0 as i64, &city.to_string());
+                bot.send_message(msg.chat.id,"Yes.").await?;
+                let _ = dialogue.exit().await;
+            }
+            None => {
+                bot.send_message(msg.chat.id, "No city specified, try again.").await?;
+            }
         }
     }
     Ok(())
