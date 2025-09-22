7b26334422944a5d1627c0206fd3cf312a015aa9
Andriy <114863893+noinsts@users.noreply.github.com>
2025-09-22 11:41:17 +0300

    feat(handlers): integrate OpenWeather API into today_handler with city lookup from DB

--- src/handlers/today.rs
+++ src/handlers/today.rs
@@ -1,12 +1,58 @@
+use std::env;
+use dotenvy::dotenv;
 use teloxide::prelude::*;
 use teloxide::Bot;
 
 use crate::db::db::Db;
+use crate::db::queries::get_city;
 use crate::types::HandlerResult;
+use crate::api::{fetch_forecast, today_weather};
 
-pub async fn today_handler(bot: Bot, callback: CallbackQuery, db: Db) -> HandlerResult {
+pub async fn today_handler(bot: Bot, callback: CallbackQuery, db: &Db) -> HandlerResult {
+    dotenv().ok();
+
+    let token = match env::var("WEATHER_API_KEY") {
+        Ok(v) => v,
+        Err(_) => {
+            eprintln!("WEATHER_API_KEY environment variable not set");
+            bot.answer_callback_query(callback.id)
+                .text("Помилка, зверніться до розробників.")
+                .await?;
+            return Ok(())
+        }
+    };
+
+    let city = match get_city(&db, callback.from.id.0 as i64) {
+        Some(c) => c.to_string(),
+        None => {
+            bot.answer_callback_query(callback.id)
+                .text("Ваше рідне місто не знайдено. Спробуйте знову.")
+                .await?;
+            return Ok(());
+        }
+    };
+
     if let Some(message) = callback.message {
-        bot.send_message(message.chat().id, "Weather today!").await?;
+        match fetch_forecast(&city, &token).await {
+            Ok(resp) => {
+                if let Some(today) = today_weather(&resp) {
+                    bot.send_message(
+                        message.chat().id,
+                        format!("Сьогодні: {}, {}", today.main.temp, today.weather[0].description)
+                    )
+                        .await?;
+                }
+                else {
+                    bot.send_message(message.chat().id, "Не вдалося отримати прогноз погоди на сьогодні")
+                        .await?;
+
+                }
+            }
+            Err(_) => {
+                bot.send_message(message.chat().id, "Помилка при отриманні даних про погоду.")
+                    .await?;
+            }
+        }
     }
     bot.answer_callback_query(callback.id).await?;
     Ok(())
