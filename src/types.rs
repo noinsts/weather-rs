14fec6a8e2b11f048c39e2a81a3d4256a3b35c59
Andriy <114863893+noinsts@users.noreply.github.com>
2025-09-27 23:26:43 +0300

    docs: add documentation to types.rs

--- src/types.rs
+++ src/types.rs
@@ -1,6 +1,10 @@
 use teloxide::dispatching::dialogue::InMemStorage;
 use teloxide::prelude::Dialogue;
+
 use crate::states::State;
 
+/// Dialogue type alias.
 pub type MyDialogue = Dialogue<State, InMemStorage<State>>;
+
+/// Standard result type alias for bot handlers.
 pub type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
