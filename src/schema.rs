b2ab07994e19650236fbd002368e653752157fca
Andriy <114863893+noinsts@users.noreply.github.com>
2025-10-03 20:42:28 +0300

    feat(schema): add ukrainian handler and english handler to schema

--- src/schema.rs
+++ src/schema.rs
@@ -49,6 +49,14 @@ pub fn schema() -> UpdateHandler<Box<dyn std::error::Error + Send + Sync + 'stat
         .branch(
             dptree::filter(|q: CallbackQuery| q.data.as_deref() == Some(Callbacks::SelectLanguage.as_str()))
                 .endpoint(settings::language::hub::handler),
+        )
+        .branch(
+            dptree::filter(|q: CallbackQuery| q.data.as_deref() == Some(Callbacks::Ukrainian.as_str()))
+                .endpoint(settings::language::ukrainian_handler)
+        )
+        .branch(
+            dptree::filter(|q: CallbackQuery| q.data.as_deref() == Some(Callbacks::English.as_str()))
+                .endpoint(settings::language::english_handler)
         );
 
     dptree::entry()
