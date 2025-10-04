2178e6ca77d7d18d4a17bddbdfd88d4844a3ea07
Andriy <114863893+noinsts@users.noreply.github.com>
2025-10-04 15:06:31 +0300

    feat(settings): add DbPool parameter to callback handlers and pass it to the universal handler function

--- src/handlers/settings/language/select.rs
+++ src/handlers/settings/language/select.rs
@@ -1,18 +1,18 @@
 use teloxide::prelude::*;
 use teloxide::types::CallbackQuery;
 
+use crate::db::pool::DbPool;
 use crate::enums::languages::Languages;
 use crate::types::HandlerResult;
 
-async fn handler(bot: Bot, callbacks: CallbackQuery, lang: Languages) -> HandlerResult {
-    print!("hello!\n");
+async fn handler(bot: Bot, callback: CallbackQuery, pool: DbPool, lang: Languages) -> HandlerResult {
     Ok(())
 }
 
-pub async fn ukrainian_handler(bot: Bot, callback: CallbackQuery) -> HandlerResult {
-    handler(bot, callback, Languages::Uk).await
+pub async fn ukrainian_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
+    handler(bot, callback, db, Languages::Uk).await
 }
 
-pub async fn english_handler(bot: Bot, callback: CallbackQuery) -> HandlerResult {
-    handler(bot, callback, Languages::En).await
+pub async fn english_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
+    handler(bot, callback, db, Languages::En).await
 }
