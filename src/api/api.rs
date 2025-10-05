ca75cbb43af1fe4c368620093f0f030d730362bf
Andriy <114863893+noinsts@users.noreply.github.com>
2025-10-05 14:46:02 +0300

    feat(api): add localization to request api

--- src/api/api.rs
+++ src/api/api.rs
@@ -1,9 +1,10 @@
 use crate::api::models::WeatherResponse;
+use crate::enums::languages::Languages;
 
-pub async fn fetch_forecast(city: &str, api_key: &str) -> Result<WeatherResponse, reqwest::Error> {
+pub async fn fetch_forecast(city: &str, api_key: &str, lang: Languages) -> Result<WeatherResponse, reqwest::Error> {
     let url = format!(
-        "https://api.openweathermap.org/data/2.5/forecast?q={}&appid={}&units=metric&lang=ua",
-        city, api_key
+        "https://api.openweathermap.org/data/2.5/forecast?q={}&appid={}&units=metric&lang={}",
+        city, api_key, lang.as_str()
     );
 
     let resp = reqwest::get(&url).await?.json::<WeatherResponse>().await?;
