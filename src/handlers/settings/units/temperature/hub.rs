790622c05945e4469ecd851e166fc1109f6fd365
Andriy <114863893+noinsts@users.noreply.github.com>
2025-10-09 23:40:00 +0300

    feat(settings): add support of reply keyboard in units/temperature/hub handler

--- src/handlers/settings/units/temperature/hub.rs
+++ src/handlers/settings/units/temperature/hub.rs
@@ -6,6 +6,7 @@ use crate::db::queries::UserQueries;
 use crate::enums::languages::Languages;
 use crate::traits::chat::ChatSource;
 use crate::types::HandlerResult;
+use crate::utils::keyboard::get_temperature_keyboard;
 use crate::utils::locales::get_text;
 
 pub async fn handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
@@ -22,6 +23,7 @@ pub async fn handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerRe
 
         bot.edit_message_text(chat_id, message_id, get_text(lang, "settings-units-temp-hub", None))
             .parse_mode(ParseMode::Html)
+            .reply_markup(get_temperature_keyboard(lang))
             .await?;
     }
     Ok(())
