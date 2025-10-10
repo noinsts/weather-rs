b70f3cd5cfea8a959d4daaa35db32dfa67340374
Andriy <114863893+noinsts@users.noreply.github.com>
2025-10-10 23:09:37 +0300

    feat(settings): added hub_handler support to measurement units hub

--- src/handlers/settings/units/hub.rs
+++ src/handlers/settings/units/hub.rs
@@ -1,31 +1,17 @@
 use teloxide::prelude::*;
-use teloxide::types::{CallbackQuery, ParseMode};
+use teloxide::types::CallbackQuery;
 
 use crate::db::pool::DbPool;
-use crate::db::queries::UserQueries;
-use crate::enums::languages::Languages;
-use crate::traits::chat::ChatSource;
 use crate::types::HandlerResult;
+use crate::utils::chat::hub_handler;
 use crate::utils::keyboard::get_units_keyboard;
-use crate::utils::locales::get_text;
 
 pub async fn handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
-    let user = UserQueries::get_user(&db, callback.user_id()).await;
-
-    if let Some(message) = callback.message {
-        let chat_id = message.chat().id;
-        let message_id = message.id();
-
-        let lang = user
-            .as_ref()
-            .and_then(|u| Languages::from_str(&u.language))
-            .unwrap_or_default();
-
-        bot.edit_message_text(chat_id, message_id, get_text(lang, "settings-units-hub", None))
-            .parse_mode(ParseMode::Html)
-            .reply_markup(get_units_keyboard(lang))
-            .await?;
-    }
-
-    Ok(())
+    hub_handler(
+        &bot, 
+        &callback, 
+        &db, 
+        "settings-units-hub", 
+        get_units_keyboard
+    ).await
 }
