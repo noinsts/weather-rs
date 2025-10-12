810ce1f30bcba480eaa0a94f05d20ddfdac4165b
Andriy <114863893+noinsts@users.noreply.github.com>
2025-10-12 23:38:09 +0300

    feat(settings): implement logic for select temperature unit

--- src/handlers/settings/units/temperature/select.rs
+++ src/handlers/settings/units/temperature/select.rs
@@ -2,11 +2,48 @@ use teloxide::prelude::*;
 use teloxide::types::CallbackQuery;
 
 use crate::db::pool::DbPool;
+use crate::db::queries::UserQueries;
+use crate::enums::languages::Languages;
 use crate::enums::units::TemperatureUnits;
+use crate::traits::chat::ChatSource;
 use crate::types::HandlerResult;
 
 async fn handler(bot: Bot, callback: CallbackQuery, db: DbPool, unit: TemperatureUnits) -> HandlerResult {
     println!("Temperature select handler called");
+
+    let user_id = callback.user_id();
+    let callback_id = callback.id.clone();
+
+    let text = match UserQueries::get_user(&db, user_id).await {
+        Some(user) => {
+            // let lang = Languages::from_str(&user.language);
+
+            if TemperatureUnits::from_str(&user.temperature_unit.as_str()) == Some(unit) {
+                "Нічого не змінилось".to_string()
+            }
+            else {
+                match UserQueries::set_temp_unit(&db, user_id, unit.as_str()).await {
+                    Ok(_) => {
+                        "Все чудово!".to_string()
+                    }
+                    Err(_) => {
+                        "Помилка".to_string()
+                    }
+                }
+            }
+
+            // something
+        }
+        None => {
+            "Помилка".to_string()
+        }
+    };
+
+    bot.answer_callback_query(callback_id)
+        .text(text)
+        .show_alert(true)
+        .await?;
+
     Ok(())
 }
 
