e2c32c52239668632860989a653ddad45e3bf839
Andriy <114863893+noinsts@users.noreply.github.com>
2025-10-12 23:53:47 +0300

    feat(settings): add localized messages for temperature unit changes in callback handler

--- src/handlers/settings/units/temperature/select.rs
+++ src/handlers/settings/units/temperature/select.rs
@@ -7,36 +7,27 @@ use crate::enums::languages::Languages;
 use crate::enums::units::TemperatureUnits;
 use crate::traits::chat::ChatSource;
 use crate::types::HandlerResult;
+use crate::utils::locales::get_text;
 
 async fn handler(bot: Bot, callback: CallbackQuery, db: DbPool, unit: TemperatureUnits) -> HandlerResult {
-    println!("Temperature select handler called");
-
     let user_id = callback.user_id();
     let callback_id = callback.id.clone();
 
     let text = match UserQueries::get_user(&db, user_id).await {
         Some(user) => {
-            // let lang = Languages::from_str(&user.language);
+            let lang = Languages::from_str(&user.language).unwrap_or_default();
 
             if TemperatureUnits::from_str(&user.temperature_unit.as_str()) == Some(unit) {
-                "Нічого не змінилось".to_string()
+                get_text(lang, "temperature-unit-no-change", None)
             }
             else {
                 match UserQueries::set_temp_unit(&db, user_id, unit.as_str()).await {
-                    Ok(_) => {
-                        "Все чудово!".to_string()
-                    }
-                    Err(_) => {
-                        "Помилка".to_string()
-                    }
+                    Ok(_) => get_text(lang, "temperature-unit-success", None),
+                    Err(_) => get_text(lang, "error", None)
                 }
             }
-
-            // something
         }
-        None => {
-            "Помилка".to_string()
-        }
+        None => get_text(Languages::default(), "error", None)
     };
 
     bot.answer_callback_query(callback_id)
