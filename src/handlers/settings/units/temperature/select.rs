64dc2d907e3a6ddcd8de612acfdc93c36168ce70
Andriy <114863893+noinsts@users.noreply.github.com>
2025-10-12 23:01:54 +0300

    refactor(settings): unify temperature units handlers into a single generic handler with TemperatureUnits enum

--- src/handlers/settings/units/temperature/select.rs
+++ src/handlers/settings/units/temperature/select.rs
@@ -2,19 +2,22 @@ use teloxide::prelude::*;
 use teloxide::types::CallbackQuery;
 
 use crate::db::pool::DbPool;
+use crate::enums::units::TemperatureUnits;
 use crate::types::HandlerResult;
 
-pub async fn celsius_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
-    println!("Temperature select called");
+async fn handler(bot: Bot, callback: CallbackQuery, db: DbPool, unit: TemperatureUnits) -> HandlerResult {
+    println!("Temperature select handler called");
     Ok(())
+}
+
+pub async fn celsius_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
+    handler(bot, callback, db, TemperatureUnits::Celsius).await
 }
 
 pub async fn fahrenheit_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
-    println!("Temperature select called");
-    Ok(())
+    handler(bot, callback, db, TemperatureUnits::Fahrenheit).await
 }
 
 pub async fn kelvin_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
-    println!("Temperature select called");
-    Ok(())
+    handler(bot, callback, db, TemperatureUnits::Kelvin).await
 }
