6e484bd5769dabbc31b06b63a46195dce39a3400
Andriy <114863893+noinsts@users.noreply.github.com>
2025-10-14 17:52:30 +0300

    refactor(settings): unify select speed unit handlers through shared handler function

--- src/handlers/settings/units/speed/select.rs
+++ src/handlers/settings/units/speed/select.rs
@@ -1,12 +1,25 @@
 use teloxide::prelude::*;
 
 use crate::db::pool::DbPool;
+use crate::enums::units::SpeedUnits;
 use crate::types::HandlerResult;
 
-pub async fn khp_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult { Ok(()) }
+async fn handler(bot: Bot, callback: CallbackQuery, db: DbPool, unit: SpeedUnits) -> HandlerResult {
+    Ok(())
+}
 
-pub async fn mps_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult { Ok(()) }
+pub async fn khp_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
+    handler(bot, callback, db, SpeedUnits::KilometersPerHour).await
+}
 
-pub async fn mph_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult { Ok(()) }
+pub async fn mps_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
+     handler(bot, callback, db, SpeedUnits::MetersPerSecond).await
+}
 
-pub async fn knots_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult { Ok(()) }
+pub async fn mph_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
+    handler(bot, callback, db, SpeedUnits::MilesPerHour).await
+}
+
+pub async fn knots_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
+    handler(bot, callback, db, SpeedUnits::Knots).await
+}
