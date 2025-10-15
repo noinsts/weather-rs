fbac17cdc4bb364539fb2de8e9d2ef8e39c099fc
Andriy <114863893+noinsts@users.noreply.github.com>
2025-10-15 17:01:02 +0300

    docs(settings): add docs to select speed unit handlers

--- src/handlers/settings/units/speed/select.rs
+++ src/handlers/settings/units/speed/select.rs
@@ -9,6 +9,22 @@ use crate::traits::chat::ChatSource;
 use crate::types::HandlerResult;
 use crate::utils::locales::get_text;
 
+/// Handles the logic of updating user's preferred speed unit.
+///
+/// # Arguments
+/// - `bot` - The Telegram bot instance.
+/// - `callback` - The callback query triggered for user.
+/// - `db` - Shared database connection pool.
+/// - `unit` - The speed unit to be set.
+///
+/// # Returns
+/// - [`HandlerResult`] - Result indication whether the handler executed successfully.
+///
+/// # Behavior
+/// - Fetches the user from the database.
+/// - If the user already has the selected temperature unit, sends "no change" message.
+/// - Otherwise, update's the user temperature unit and send success message.
+/// - Already replies with a callback alert.
 async fn handler(bot: Bot, callback: CallbackQuery, db: DbPool, unit: SpeedUnits) -> HandlerResult {
     let user_id = callback.user_id();
     let callback_id = callback.id.clone();
@@ -38,18 +54,22 @@ async fn handler(bot: Bot, callback: CallbackQuery, db: DbPool, unit: SpeedUnits
     Ok(())
 }
 
+/// Handles the callback to selecting **KilometersPerHour** as the speed unit.
 pub async fn khp_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
     handler(bot, callback, db, SpeedUnits::KilometersPerHour).await
 }
 
+/// Handles the callback to selecting **MetersPerSecond** as the speed unit.
 pub async fn mps_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
      handler(bot, callback, db, SpeedUnits::MetersPerSecond).await
 }
 
+/// Handles the callback to selecting **MilesPerHour** as the speed unit.
 pub async fn mph_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
     handler(bot, callback, db, SpeedUnits::MilesPerHour).await
 }
 
+/// Handles the callback to selecting **Knots** as the speed unit.
 pub async fn knots_handler(bot: Bot, callback: CallbackQuery, db: DbPool) -> HandlerResult {
     handler(bot, callback, db, SpeedUnits::Knots).await
 }
